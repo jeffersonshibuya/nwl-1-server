ca6b3a264dcba0ff3d55fdbbb0afa96ca2e87bfe
Jefferson Shibuya <jeffersonshibuya@yahoo.com.br>
2020-06-06 16:18:44 -0300

    Postgres

--- knexfile.ts
+++ knexfile.ts
@@ -17,7 +17,10 @@ import path from "path";
 module.exports = {
   client: "pg",
   connection: process.env.DATABASE_URL,
+  // connection:
+  //   "postgres://npjtybqnzxgevs:cb38063583b8195d076da29615c3fdd3e96bdd158f3a2ed4f43b94b51e22ec01@ec2-34-193-117-204.compute-1.amazonaws.com:5432/ddjlaktsksftlc?ssl=no-verify",
   searchPath: ["knex", "public"],
+  ssl: { rejectUnauthorized: false },
   // connection: {
   //   database: "ecoleta",
   //   user: "postgres",
