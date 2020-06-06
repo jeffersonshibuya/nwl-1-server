901a663183a91dfdb740e5f455e2f42f56800c2b
Jefferson Shibuya <jeffersonshibuya@yahoo.com.br>
2020-06-06 16:05:55 -0300

    Postgres

--- knexfile.ts
+++ knexfile.ts
@@ -1,10 +1,28 @@
 import path from "path";
 
+// module.exports = {
+//   client: "sqlite3",
+//   connection: {
+//     filename: path.resolve(__dirname, "src", "database", "database.sqlite"),
+//   },
+//   migrations: {
+//     directory: path.resolve(__dirname, "src", "database", "migrations"),
+//   },
+//   seeds: {
+//     directory: path.resolve(__dirname, "src", "database", "seeds"),
+//   },
+//   useNullAsDefault: true,
+// };
+
 module.exports = {
-  client: "sqlite3",
-  connection: {
-    filename: path.resolve(__dirname, "src", "database", "database.sqlite"),
-  },
+  client: "pg",
+  connection: process.env.DATABASE_URL,
+  searchPath: ["knex", "public"],
+  // connection: {
+  //   database: "ecoleta",
+  //   user: "postgres",
+  //   password: "docker",
+  // },
   migrations: {
     directory: path.resolve(__dirname, "src", "database", "migrations"),
   },
--- src/database/connection.ts
+++ src/database/connection.ts
@@ -1,10 +1,20 @@
 import knex from "knex";
 import path from "path";
 
+// const connection = knex({
+//   client: "sqlite3",
+//   connection: {
+//     filename: path.resolve(__dirname, "database.sqlite"),
+//   },
+//   useNullAsDefault: true,
+// });
+
 const connection = knex({
-  client: "sqlite3",
+  client: "pg",
   connection: {
-    filename: path.resolve(__dirname, "database.sqlite"),
+    database: "ecoleta",
+    user: "postgres",
+    password: "docker",
   },
   useNullAsDefault: true,
 });
